c0b77b5cce7473c3232c8a5462ceccb4aab132b2
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-17 14:47:54 +0100

    populate football teams with random stats

--- main.js
+++ main.js
@@ -35,25 +35,12 @@ let lowestBike = bicycles[0];
 for (let i = 1; i < bicycles.length; i++) {
   const bicycle = bicycles[i];
   if (bicycle.weight < lowestBike.weight) {
-    lowestBike = bicycle
-    
+    lowestBike = bicycle;
   }
 }
-// console.log(lowestBike.name);
 
-const lowestBikeEl = document.getElementById('snack_1')
-
+const lowestBikeEl = document.getElementById("snack_1");
 lowestBikeEl.textContent = `${lowestBike.name}, ${lowestBike.weight} kg`;
-
-
-
-
-
-
-
-
-
-
 
 /* 
 Snack 2
@@ -63,3 +50,43 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
+
+const footballTeams = [
+  {
+    name: "Juventus FC",
+    points: 0,
+    fouls: 0,
+  },
+  {
+    name: "AC Milan",
+    points: 0,
+    fouls: 0,
+  },
+  {
+    name: "Inter Milan",
+    points: 0,
+    fouls: 0,
+  },
+  {
+    name: "SSC Napoli",
+    points: 0,
+    fouls: 0,
+  },
+  {
+    name: "AS Roma",
+    points: 0,
+    fouls: 0,
+  },
+];
+
+function getRandomNum(min, max) {
+  return Math.floor(Math.random() * (max - min) ) + min;
+}
+
+for (let i = 0; i < footballTeams.length; i++){
+    const footballTeam = footballTeams[i]
+    footballTeam.points = getRandomNum(1, 100)
+    footballTeam.fouls = getRandomNum(100, 200)
+    console.log(footballTeam);
+    
+}
