7877d3acc23ced3e8c63a830c75635783ff95544
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-17 13:55:05 +0100

    complete snack 1

--- main.js
+++ main.js
@@ -1,5 +1,4 @@
-console.log('it works');
-
+console.log("it works");
 
 /* 
 Snack 1
@@ -9,37 +8,42 @@ Stampare a schermo la bici con peso minore.
 */
 
 const bicycles = [
-    {
-        name: 'Road Bike',
-        weight: 7.5
-    },
-        {
-        name: 'Mountain Bike',
-        weight: 13
-    },
-        {
-        name: 'City Bike',
-        weight: 16
-    },
-        {
-        name: 'E-Bike',
-        weight: 24
-    },
-        {
-        name: 'Foldable Bike',
-        weight: 11
-    },
-]
+  {
+    name: "E-Bike",
+    weight: 24,
+  },
+  {
+    name: "Mountain Bike",
+    weight: 13,
+  },
+  {
+    name: "City Bike",
+    weight: 16,
+  },
+  {
+    name: "Road Bike",
+    weight: 7.5,
+  },
+  {
+    name: "Foldable Bike",
+    weight: 11,
+  },
+];
 
+let lowestBike = bicycles[0];
 
+for (let i = 1; i < bicycles.length; i++) {
+  const bicycle = bicycles[i];
+  if (bicycle.weight < lowestBike.weight) {
+    lowestBike = bicycle
+    
+  }
+}
+// console.log(lowestBike.name);
 
+const lowestBikeEl = document.getElementById('snack_1')
 
-
-
-
-
-
-
+lowestBikeEl.textContent = `${lowestBike.name}, ${lowestBike.weight} kg`;
 
 
 
@@ -59,4 +63,3 @@ Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0
 Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
 Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
 */
-
