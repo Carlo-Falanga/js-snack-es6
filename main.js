f98029d8ab9aa0a84bf47784633404263e83a4d1
Carlo-Falanga <carlofalanga7@gmail.com>
2026-02-17 13:05:03 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,62 @@
+console.log('it works');
+
+
+/* 
+Snack 1
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore.
+*/
+
+const bicycles = [
+    {
+        name: 'Road Bike',
+        weight: 7.5
+    },
+        {
+        name: 'Mountain Bike',
+        weight: 13
+    },
+        {
+        name: 'City Bike',
+        weight: 16
+    },
+        {
+        name: 'E-Bike',
+        weight: 24
+    },
+        {
+        name: 'Foldable Bike',
+        weight: 11
+    },
+]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* 
+Snack 2
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà: nome, punti fatti, falli subiti.
+Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà “punti” fatti e “falli subiti”.
+Infine, creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti e stampiamo tutto in console.
+*/
+
